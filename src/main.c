1f044bcb5ef97c5b49270ebc5f2e6617e04abe7b
WyvernAllow <bloodflame2535@gmail.com>
2025-07-11 23:31:37 +0200

    Basic window.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+static void error_callback(int error_code, const char *description) {
+    (void)error_code;
+    fprintf(stderr, "GLFW: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    (void)scancode;
+    (void)mods;
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stderr, "glfwInit() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "Quadcraft", monitor, NULL);
+    if (!window) {
+        fprintf(stderr, "glfwCreateWindow() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        fprintf(stderr, "gladLoadGL() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(0.39, 0.58, 0.93, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
