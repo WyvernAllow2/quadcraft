cd3dd2201ab19cbddfb5419710ab17d93619cb53
WyvernAllow <bloodflame2535@gmail.com>
2025-07-12 10:06:40 +0200

    First chunk.

--- src/main.c
+++ src/main.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "direction.h"
+
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/gl.h>
 
+#include "block_type.h"
 #include "camera.h"
+#include "chunk.h"
 #include "math3d.h"
 #include "utils.h"
 
@@ -124,6 +128,103 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
     }
 }
 
+#define MAX_VISIBLE_FACES ((CHUNK_VOLUME / 2) * 6)
+#define MAX_VERTICES (MAX_VISIBLE_FACES * 4)
+#define MAX_INDICES (MAX_VISIBLE_FACES * 6)
+
+/* clang-format off */
+static const Vec3 FACE_VERTEX_TABLE[DIRECTION_COUNT][4] = {
+    [DIR_POS_X] = {
+        {1.0f, 1.0f, 0.0f}, 
+        {1.0f, 0.0f, 0.0f}, 
+        {1.0f, 0.0f, 1.0f}, 
+        {1.0f, 1.0f, 1.0f}, 
+    },
+    [DIR_POS_Y] = {
+        {0.0f, 1.0f, 1.0f}, 
+        {0.0f, 1.0f, 0.0f}, 
+        {1.0f, 1.0f, 0.0f}, 
+        {1.0f, 1.0f, 1.0f}, 
+    },
+    [DIR_POS_Z] = {
+        {1.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 1.0f},
+        {0.0f, 0.0f, 1.0f},
+        {0.0f, 1.0f, 1.0f},
+    },
+    [DIR_NEG_X] = {
+        {0.0f, 1.0f, 1.0f}, 
+        {0.0f, 0.0f, 1.0f}, 
+        {0.0f, 0.0f, 0.0f}, 
+        {0.0f, 1.0f, 0.0f}, 
+    },
+    [DIR_NEG_Y] = {
+        {0.0f, 0.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f},
+        {1.0f, 0.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f},
+    },
+    [DIR_NEG_Z] = {
+        {0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 0.0f},
+        {1.0f, 0.0f, 0.0f},
+        {1.0f, 1.0f, 0.0f}, 
+    },
+};
+/* clang-format on */
+
+Block_Vertex *vertices = NULL;
+size_t vertex_count = 0;
+
+static void mesh_block(const Chunk *chunk, iVec3 pos) {
+    Block_Type current_block = chunk->blocks[chunk_index(pos)];
+    if (current_block == BLOCK_AIR) {
+        return;
+    }
+
+    for (Direction dir = 0; dir < DIRECTION_COUNT; dir++) {
+        iVec3 normal = direction_to_ivec3(dir);
+        Block_Type neighbor = chunk_get_block(chunk, ivec3_add(pos, normal));
+        const Block_Properties *neighbor_properties = get_block_properties(neighbor);
+
+        Vec3 fpos = {pos.x, pos.y, pos.z};
+
+        if (neighbor_properties->is_transparent) {
+            vertices[vertex_count++] = (Block_Vertex){
+                .position = vec3_add(fpos, FACE_VERTEX_TABLE[dir][0]),
+            };
+
+            vertices[vertex_count++] = (Block_Vertex){
+                .position = vec3_add(fpos, FACE_VERTEX_TABLE[dir][1]),
+            };
+
+            vertices[vertex_count++] = (Block_Vertex){
+                .position = vec3_add(fpos, FACE_VERTEX_TABLE[dir][2]),
+            };
+
+            vertices[vertex_count++] = (Block_Vertex){
+                .position = vec3_add(fpos, FACE_VERTEX_TABLE[dir][3]),
+            };
+        }
+    }
+}
+
+static void mesh_chunk(const Chunk *chunk) {
+    if (!vertices) {
+        vertices = malloc(sizeof(Block_Vertex) * MAX_VERTICES);
+    }
+
+    vertex_count = 0;
+
+    for (int z = 0; z < CHUNK_SIZE_Z; z++) {
+        for (int y = 0; y < CHUNK_SIZE_Y; y++) {
+            for (int x = 0; x < CHUNK_SIZE_X; x++) {
+                mesh_block(chunk, (iVec3){x, y, z});
+            }
+        }
+    }
+}
+
 int main(void) {
     glfwSetErrorCallback(error_callback);
     if (!glfwInit()) {
@@ -159,16 +260,29 @@ int main(void) {
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, cursor_callback);
 
-    Block_Vertex vertices[] = {
-        {0.0f, 0.0f, 0.0f},
-        {0.0f, 1.0f, 0.0f},
-        {1.0f, 1.0f, 0.0f},
-        {1.0f, 0.0f, 0.0f},
-    };
+    Chunk chunk = {0};
+    for (int z = 0; z < CHUNK_SIZE_Z; z++) {
+        for (int y = 0; y < CHUNK_SIZE_Y; y++) {
+            for (int x = 0; x < CHUNK_SIZE_X; x++) {
+                chunk.blocks[chunk_index((iVec3){x, y, z})] = BLOCK_DIRT;
+            }
+        }
+    }
 
-    uint32_t indices[] = {
-        0, 1, 3, 1, 2, 3,
-    };
+    mesh_chunk(&chunk);
+
+    uint32_t *indices = malloc(sizeof(uint32_t) * MAX_INDICES);
+    uint32_t offset = 0;
+    for (uint32_t i = 0; i < MAX_INDICES; i += 6) {
+        indices[i + 0] = offset + 3;
+        indices[i + 1] = offset + 2;
+        indices[i + 2] = offset + 1;
+        indices[i + 3] = offset + 3;
+        indices[i + 4] = offset + 1;
+        indices[i + 5] = offset + 0;
+
+        offset += 4;
+    }
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -177,12 +291,12 @@ int main(void) {
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Block_Vertex) * 4, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Block_Vertex) * vertex_count, vertices, GL_STATIC_DRAW);
 
     GLuint ebo;
     glGenBuffers(1, &ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 6, indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * MAX_INDICES, indices, GL_STATIC_DRAW);
 
     /* Position attribute */
     glEnableVertexAttribArray(0);
@@ -204,6 +318,9 @@ int main(void) {
         .near = 0.01f,
         .far = 1000.0f,
     };
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
 
     float current_time = (float)glfwGetTime();
     while (!glfwWindowShouldClose(window)) {
@@ -243,7 +360,7 @@ int main(void) {
         glUniformMatrix4fv(glGetUniformLocation(program, "u_proj"), 1, GL_FALSE, camera.proj.data);
 
         glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+        glDrawElements(GL_TRIANGLES, (vertex_count / 4) * 6, GL_UNSIGNED_INT, NULL);
 
         glUseProgram(0);
 
