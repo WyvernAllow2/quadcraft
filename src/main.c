dc54bcbc0f67e8f5507904049e7a606465d79859
WyvernAllow <bloodflame2535@gmail.com>
2025-10-06 15:18:11 +0200

    Window.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,80 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+struct {
+    int window_w;
+    int window_h;
+
+    GLFWwindow *window;
+} state;
+
+static void error_callback(int error_code, const char *description) {
+    (void)error_code;
+    fprintf(stderr, "GLFW: %s\n", description);
+}
+
+static void window_size_callback(GLFWwindow* window, int width, int height) {
+    (void)window;
+    assert(window == state.window);
+
+    state.window_w = width;
+    state.window_h = height;
+    glViewport(0, 0, state.window_w, state.window_h);
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stderr, "glfwInit() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+
+    state.window_w = mode->width;
+    state.window_h = mode->height;
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+    state.window = glfwCreateWindow(state.window_w, state.window_h, "Quadcraft", monitor, NULL);
+    if (!state.window) {
+        fprintf(stderr, "glfwCreateWindow() failed\n");
+        return EXIT_FAILURE;
+    }
+    
+    glfwMakeContextCurrent(state.window);
+
+    if(!gladLoadGL(glfwGetProcAddress)) {
+        fprintf(stderr, "gladLoadGL() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetWindowSizeCallback(state.window, window_size_callback);
+
+    float old_time = glfwGetTime();
+    while(!glfwWindowShouldClose(state.window)) {
+        glfwPollEvents();
+
+        float new_time = glfwGetTime();
+        float delta_time = new_time - old_time;
+        old_time = new_time;
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(state.window);
+    }
+
+    glfwDestroyWindow(state.window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
